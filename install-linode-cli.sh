a5237736e71098007fdd3b3562078fed8b7f0948
TH3-F001 <thef00l@f00lsgold.com>
2023-12-05 04:54:46 -0800

    and another

--- install-linode-cli.sh
+++ install-linode-cli.sh
@@ -5,20 +5,23 @@ LIB_SCRIPT_DIR="$SCRIPT_DIR/libraries"
 
 source "$LIB_SCRIPT_DIR/basic-operations.lib"
 
+VENV_DIR="$HOME/.local/pipx/venvs/linode-cli/"
+CONF_DIR="$HOME/.config/linode"
+TOKEN_FILE="$CONF_DIR/token"
+
 echo -e "\n📦 Installing linode-cli."
 if ! command_exists linode-cli; then
-    VENV_DIR="$HOME/.local/pipx/venvs/linode-cli/"
-    CONF_DIR="$HOME/.config/linode"
-    TOKEN_FILE="$CONF_DIR/token"
-
     pipx install linode-cli
     "$VENV_DIR/bin/python3" -m pip install boto3
+else
+    echo -e "\t✨ Linode-CLI is already installed!"
+fi
+
+if ! file_exists $TOKEN_FILE; then
     read -p "Please provide you're Linode Access Token: " TOKEN
     mkdir -p "$CONF_DIR"
     echo "TOKEN=$TOKEN" > "$TOKEN_FILE"
     chmod 600 "$TOKEN_FILE"
-else
-    echo -e "\t✨ Linode-CLI is already installed!"
 fi
 
 if command_exists linode-cli && file_exists "$TOKEN_FILE"; then
